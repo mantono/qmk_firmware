79a6a6d4b15343c76f0bc572dd14c348f48a00d6
Anton Österberg <anton.osterberg@protonmail.com>
2025-07-02 00:29:05 +0200

    Revert "emulate vim movements on nav layer"
    This reverts commit e7f8da4bc7cd80f081c1cb956d5758e356548922.

--- keyboards/splitkb/aurora/lily58/keymaps/qwerty_dfjk/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/qwerty_dfjk/keymap.c
@@ -98,13 +98,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXX, KC_Q, TD(TD_W_AA), TD(TD_E_AE), KC_R, KC_T,                                        KC_Y, KC_U, KC_I, TD(TD_O_OE), KC_P, XXXX,
         KC_ESC, LCTL_T(KC_A), LSFT_T(KC_S), KC_D, KC_F, KC_G,                                    KC_H, KC_J, KC_K, RSFT_T(KC_L), RCTL_T(KC_SCLN), KC_ENT,
         KC_TAB, KC_Z, KC_X, KC_C, KC_V, KC_B, MEH(KC_LEFT),                        MEH(KC_RGHT), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, LALT(KC_TAB),
-                CW_TOGG, KC_LALT, MO(3), LGUI_T(KC_SPC),                           LT(2, KC_BSPC), TG(1), KC_RALT, KC_DEL
+                CW_TOGG, KC_LALT, MO(3), LGUI_T(KC_SPC),                           LT(1, KC_BSPC), OSL(2), KC_RALT, KC_DEL
     ),
 
     [1] = LAYOUT(
         XXXX, XXXX, XXXX, XXXX, XXXX, XXXX,                                                     XXXX, XXXX, XXXX, XXXX, XXXX, XXXX,
-        XXXX, XXXX, XXXX, KC_DEL, XXXX, XXXX,                                                   LCTL(KC_Z), LCTL(KC_Z), TG(1), XXXX, LCTL(KC_P), XXXX,
-        XXXX, XXXX, KC_HOME, LCTL(KC_LEFT), LCTL(KC_RGHT), KC_END,                              KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, XXXX, ____,
+        XXXX, XXXX, XXXX, XXXX, XXXX, XXXX,                                                     XXXX, XXXX, XXXX, XXXX, XXXX, XXXX,
+        XXXX, XXXX, KC_HOME, LCTL(KC_LEFT), LCTL(KC_RGHT), KC_END,                              KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, XXXX, XXXX,
         XXXX, XXXX, XXXX, XXXX, QK_REP, XXXX, XXXX,                                       XXXX, XXXX, XXXX, XXXX, XXXX, XXXX, XXXX,
                             ____, ____, ____, ____,                                       ____, ____, ____, ____
     ),
