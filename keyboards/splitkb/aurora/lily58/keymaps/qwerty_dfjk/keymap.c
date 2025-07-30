12ca8100fe88493bfa0fceaa11f82810dd5ee890
Anton Österberg <anton.osterberg@protonmail.com>
2025-07-30 11:25:07 +0200

    Move modifiers, to home row

--- keyboards/splitkb/aurora/lily58/keymaps/qwerty_dfjk/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/qwerty_dfjk/keymap.c
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5,                                                    KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
         XXXX, KC_Q, TD(TD_W_AA), TD(TD_E_AE), KC_R, KC_T,                                        KC_Y, KC_U, KC_I, TD(TD_O_OE), KC_P, XXXX,
-        LCTL_T(KC_ESC), LT(2, KC_A), KC_S, KC_D, KC_F, KC_G,                                     KC_H, KC_J, KC_K, KC_L, LT(2, KC_SCLN), RCTL_T(KC_ENT),
+        KC_ESC, LT(2, KC_A), LSFT_T(KC_S), LALT_T(KC_D), LCTL_T(KC_F), KC_G,                   KC_H, RCTL_T(KC_J), LALT_T(KC_K), RSFT_T(KC_L), LT(2, KC_SCLN), KC_ENT,
         KC_TAB, KC_Z, KC_X, KC_C, KC_V, KC_B, MEH(KC_LEFT),                        MEH(KC_RGHT), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, LALT(KC_TAB),
                 CW_TOGG, KC_LALT, MO(3), LGUI_T(KC_SPC),                           LT(1, KC_BSPC), OSM(MOD_LSFT), KC_RALT, KC_DEL
     ),
