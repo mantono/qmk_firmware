2c4715372f7b062d353e45b337c096051dac339a
Anton Österberg <anton.osterberg@protonmail.com>
2025-08-12 11:27:30 +0200

    move modifiers back and create caps word layer

--- keyboards/splitkb/aurora/lily58/keymaps/qwerty_dfjk/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/qwerty_dfjk/keymap.c
@@ -85,17 +85,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5,                                                    KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
         XXXX, KC_Q, TD(TD_W_AA), TD(TD_E_AE), KC_R, KC_T,                                        KC_Y, KC_U, KC_I, TD(TD_O_OE), KC_P, XXXX,
-        KC_ESC, LSFT_T(KC_A), LGUI_T(KC_S), LALT_T(KC_D), LCTL_T(KC_F), KC_G,                     KC_H, RCTL_T(KC_J), LALT_T(KC_K), RGUI_T(KC_L), RSFT_T(KC_SCLN), KC_ENT,
+        KC_ESC, LT(2, KC_A), LSFT_T(KC_S), LALT_T(KC_D), LCTL_T(KC_F), KC_G,                     KC_H, RCTL_T(KC_J), LALT_T(KC_K), RSFT_T(KC_L), LT(2, KC_SCLN), KC_ENT,
         KC_TAB, KC_Z, KC_X, KC_C, KC_V, KC_B, MEH(KC_LEFT),                        MEH(KC_RGHT), KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, LALT(KC_TAB),
-                CW_TOGG, KC_LALT, LT(3, KC_ESC), LT(2, KC_SPC),                           LT(1, KC_BSPC), KC_ENT, KC_RALT, KC_DEL
+                CW_TOGG, KC_LALT, LT(3, KC_ESC), LGUI_T(KC_SPC),                           LT(1, KC_BSPC), KC_ENT, KC_RALT, KC_DEL
     ),
 
     [1] = LAYOUT(
-        XXXX, XXXX,    XXXX,    XXXX,    XXXX,    XXXX,                                   XXXX,         XXXX,    XXXX,  XXXX,     XXXX,   XXXX,
-        XXXX, XXXX,    XXXX,    XXXX,    XXXX,    XXXX,                                   XXXX,         XXXX,    XXXX,  XXXX,     XXXX,   XXXX,
-        XXXX, KC_HOME, KC_LCTL, KC_LEFT, KC_RGHT, XXXX,                                   XXXX,      KC_DOWN,   KC_UP,  KC_RIGHT, KC_END, XXXX,
-        XXXX, XXXX,    XXXX,    XXXX,    QK_REP,  XXXX,   XXXX,                     XXXX, XXXX,         XXXX,    XXXX,  XXXX,     XXXX,   XXXX,
-                                ____, ____, ____, LALT(KC_TAB),                    ____, QK_LAYER_LOCK, ____, ____
+        XXXX,     XXXX,     XXXX,     XXXX,     XXXX,            XXXX,                                   XXXX,    XXXX,    XXXX,      XXXX,     XXXX,   XXXX,
+        XXXX,     XXXX,     XXXX,     XXXX,     XXXX,            XXXX,                                   XXXX,    XXXX,    XXXX,      XXXX,     XXXX,   XXXX,
+        XXXX,  KC_HOME,  KC_LCTL,  KC_LEFT,  KC_RGHT,            XXXX,                                   XXXX, KC_DOWN,   KC_UP,  KC_RIGHT,   KC_END,   XXXX,
+        XXXX,     XXXX,     XXXX,     XXXX,   QK_REP,            XXXX,   XXXX,                     XXXX, XXXX,    XXXX,    XXXX,      XXXX,     XXXX,   XXXX,
+                            ____,     ____,     ____,   QK_LAYER_LOCK,                                   ____,    ____,    ____,      ____
     ),
 
     [2] = LAYOUT(
@@ -106,8 +106,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ____,      KC_LT,      KC_GT,    KC_LPRN,    KC_RPRN,    KC_QUOT,                          KC_DQUO,    KC_LCBR,    KC_RCBR,    KC_LBRC,    KC_RBRC, ____,
         //         :           _           |           &           ^                                 !           ?           ,            .          ;
         ____,    KC_COLN,    KC_UNDS,    KC_PIPE,    KC_AMPR,    KC_CIRC, XXXX,              XXXX, KC_EXLM,    KC_QUES,    KC_COMM,     KC_DOT,    KC_SCLN, ____,
-        //                                                         ~                                  `
-                                            ____,       ____,    KC_TILD, ____,            KC_TAB,  KC_GRV,       ____,       ____
+        //                                             ~                                                          `
+                                ____,       ____,    KC_TILD,     KC_TAB,                     LALT(KC_TAB),     KC_GRV,       ____,       ____
     ),
 
     [3] = LAYOUT(
@@ -116,15 +116,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXX, KC_1,  KC_2,  KC_3,  KC_4,  KC_5,                                                 KC_6,    KC_7,  KC_8,  KC_9,  KC_0,   XXXX,
         XXXX, XXXX,  XXXX,  XXXX,  XXXX,  XXXX, LCTL(KC_PMNS),                   LCTL(KC_PPLS), XXXX,    XXXX,  XXXX,  XXXX,  XXXX,   XXXX,
                             ____,  ____,  ____, ____,                                    ____,  ____,    ____,  ____
-    )
+    ),
+
+    [4] = LAYOUT(
+        ____,       ____,       ____,       ____,       ____,        ____,                           ____,        ____,          ____,         ____,          ____,    ____,
+        ____, LSFT(KC_Q), LSFT(KC_W), LSFT(KC_E), LSFT(KC_R),  LSFT(KC_T),                     LSFT(KC_Y),  LSFT(KC_U),    LSFT(KC_I),   LSFT(KC_O),    LSFT(KC_P),    ____,
+        ____, LSFT(KC_A), LSFT(KC_S), LSFT(KC_D), LSFT(KC_F),  LSFT(KC_G),                     LSFT(KC_H),  LSFT(KC_J),    LSFT(KC_K),   LSFT(KC_L),          XXXX,    ____,
+        ____, LSFT(KC_Z), LSFT(KC_X), LSFT(KC_C), LSFT(KC_V),  LSFT(KC_B),  ____,        ____, LSFT(KC_N),  LSFT(KC_M),          XXXX,       KC_DOT,       KC_UNDS,    ____,
+                                ____,       ____,       ____,       TG(4),                     TG(4),            TG(4),          ____,         ____
+    ),
 };
 
 // Combos
 const uint16_t PROGMEM switcher[] = {LCTL_T(KC_F), RCTL_T(KC_J), COMBO_END};
 const uint16_t PROGMEM launcher[] = {KC_G, KC_H, COMBO_END};
+const uint16_t PROGMEM caps_word[] = {LSFT_T(KC_S), RSFT_T(KC_L), COMBO_END};
 combo_t key_combos[] = {
     COMBO(switcher, LGUI(KC_H)),
     COMBO(launcher, LGUI(KC_G)), // keycodes with modifiers are possible too!
+    COMBO(caps_word, TO(4)),
 };
 
 #ifdef OTHER_KEYMAP_C
