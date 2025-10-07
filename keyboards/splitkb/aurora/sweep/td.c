9b33edefd57024708137c13d344360ab71456f19
Anton Österberg <anton.osterberg@protonmail.com>
2025-10-07 11:57:40 +0200

    Add custom Aurora Sweep firmware

--- keyboards/splitkb/aurora/sweep/td.c
+++ keyboards/splitkb/aurora/sweep/td.c
@@ -1,19 +1,3 @@
-// ö
-#define SE_OE KC_SCLN
-// ä
-#define SE_AE KC_QUOT
-// å
-#define SE_AA KC_LBRC
-
-// https://docs.qmk.fm/features/tap_dance#example-3
-enum {
-  // å/Å
-  TD_AA = 0,
-  // ä/Ä
-  TD_AE,
-  // ö/Ö
-  TD_OE
-};
 
 typedef struct {
     uint16_t tap;
@@ -51,19 +35,205 @@ void tap_dance_tap_hold_reset(tap_dance_state_t *state, void *user_data) {
 #define ACTION_TAP_DANCE_TAP_HOLD(tap, hold) \
     { .fn = {NULL, tap_dance_tap_hold_finished, tap_dance_tap_hold_reset}, .user_data = (void *)&((tap_dance_tap_hold_t){tap, hold, 0}), }
 
+// Tap Dance declarations
+enum {
+  // a -> ä
+  TD_E_AE,
+  // w -> å
+  TD_W_AA,
+  // o -> ö
+  TD_O_OE,
+  // enter -> ctrl + enter
+  TD_ENTER,
+  // ()
+  TD_PAREN,
+  // {}
+  TD_CURLY,
+  // <>
+  TD_ANGLE,
+  // []
+  TD_BRACKET,
+};
+
+typedef enum {
+    TD_NONE,
+    TD_UNKNOWN,
+    TD_SINGLE_TAP,
+    TD_SINGLE_HOLD,
+    TD_DOUBLE_TAP,
+    TD_DOUBLE_HOLD,
+    TD_DOUBLE_SINGLE_TAP, // Send two single taps
+    TD_TRIPLE_TAP,
+    TD_TRIPLE_HOLD
+} td_state_t;
+
+typedef struct {
+    bool is_press_action;
+    td_state_t state;
+} td_tap_t;
+
+td_state_t cur_dance(tap_dance_state_t *state);
+
+void paren_finished(tap_dance_state_t *state, void *user_data);
+void paren_reset(tap_dance_state_t *state, void *user_data);
+void curly_finished(tap_dance_state_t *state, void *user_data);
+void curly_reset(tap_dance_state_t *state, void *user_data);
+void angle_finished(tap_dance_state_t *state, void *user_data);
+void angle_reset(tap_dance_state_t *state, void *user_data);
+void bracket_finished(tap_dance_state_t *state, void *user_data);
+void bracket_reset(tap_dance_state_t *state, void *user_data);
+
+td_state_t cur_dance(tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed) return TD_SINGLE_TAP;
+        // Key has not been interrupted, but the key is still held. Means you want to send a 'HOLD'.
+        else return TD_SINGLE_HOLD;
+    } else if (state->count == 2) {
+        // TD_DOUBLE_SINGLE_TAP is to distinguish between typing "pepper", and actually wanting a double tap
+        // action when hitting 'pp'. Suggested use case for this return value is when you want to send two
+        // keystrokes of the key, and not the 'double tap' action/macro.
+        if (state->interrupted) return TD_DOUBLE_SINGLE_TAP;
+        else if (state->pressed) return TD_DOUBLE_HOLD;
+        else return TD_DOUBLE_TAP;
+    }
+
+    // Assumes no one is trying to type the same letter three times (at least not quickly).
+    // If your tap dance key is 'KC_W', and you want to type "www." quickly - then you will need to add
+    // an exception here to return a 'TD_TRIPLE_SINGLE_TAP', and define that enum just like 'TD_DOUBLE_SINGLE_TAP'
+    if (state->count == 3) {
+        if (state->interrupted || !state->pressed) return TD_TRIPLE_TAP;
+        else return TD_TRIPLE_HOLD;
+    } else return TD_UNKNOWN;
+}
+
+static td_tap_t paren_tap_state = {
+    .is_press_action = true,
+    .state = TD_NONE
+};
+
+static td_tap_t curly_tap_state = {
+    .is_press_action = true,
+    .state = TD_NONE
+};
+
+static td_tap_t angle_tap_state = {
+    .is_press_action = true,
+    .state = TD_NONE
+};
+
+static td_tap_t bracket_tap_state = {
+    .is_press_action = true,
+    .state = TD_NONE
+};
+
+
+void paren_finished(tap_dance_state_t *state, void *user_data) {
+    paren_tap_state.state = cur_dance(state);
+    switch (paren_tap_state.state) {
+        case TD_SINGLE_TAP: register_code16(KC_LPRN); break;
+        case TD_SINGLE_HOLD: register_code16(KC_RPRN); break;
+        case TD_DOUBLE_TAP: tap_code16(KC_LPRN); tap_code16(KC_RPRN); register_code(KC_LEFT); break;
+        default: break;
+    }
+}
+
+void paren_reset(tap_dance_state_t *state, void *user_data) {
+    switch (paren_tap_state.state) {
+        case TD_SINGLE_TAP: unregister_code16(KC_LPRN); break;
+        case TD_SINGLE_HOLD: unregister_code16(KC_RPRN); break;
+        case TD_DOUBLE_TAP: unregister_code(KC_LEFT); break;
+        case TD_DOUBLE_HOLD: break;
+        case TD_DOUBLE_SINGLE_TAP: break;
+        default: break;
+    }
+    paren_tap_state.state = TD_NONE;
+}
+
+void curly_finished(tap_dance_state_t *state, void *user_data) {
+    curly_tap_state.state = cur_dance(state);
+    switch (curly_tap_state.state) {
+        case TD_SINGLE_TAP: register_code16(KC_LCBR); break;
+        case TD_SINGLE_HOLD: register_code16(KC_RCBR); break;
+        case TD_DOUBLE_TAP: tap_code16(KC_LCBR); tap_code16(KC_RCBR); register_code(KC_LEFT); break;
+        default: break;
+    }
+}
+
+void curly_reset(tap_dance_state_t *state, void *user_data) {
+    switch (curly_tap_state.state) {
+        case TD_SINGLE_TAP: unregister_code16(KC_LCBR); break;
+        case TD_SINGLE_HOLD: unregister_code16(KC_RCBR); break;
+        case TD_DOUBLE_TAP: unregister_code(KC_LEFT); break;
+        case TD_DOUBLE_HOLD: break;
+        case TD_DOUBLE_SINGLE_TAP: break;
+        default: break;
+    }
+    curly_tap_state.state = TD_NONE;
+}
+
+void bracket_finished(tap_dance_state_t *state, void *user_data) {
+    bracket_tap_state.state = cur_dance(state);
+    switch (bracket_tap_state.state) {
+        case TD_SINGLE_TAP: register_code16(KC_LBRC); break;
+        case TD_SINGLE_HOLD: register_code16(KC_RBRC); break;
+        case TD_DOUBLE_TAP: tap_code16(KC_LBRC); tap_code16(KC_RBRC); register_code(KC_LEFT); break;
+        default: break;
+    }
+}
+
+void bracket_reset(tap_dance_state_t *state, void *user_data) {
+    switch (bracket_tap_state.state) {
+        case TD_SINGLE_TAP: unregister_code16(KC_LBRC); break;
+        case TD_SINGLE_HOLD: unregister_code16(KC_RBRC); break;
+        case TD_DOUBLE_TAP: unregister_code(KC_LEFT); break;
+        case TD_DOUBLE_HOLD: break;
+        case TD_DOUBLE_SINGLE_TAP: break;
+        default: break;
+    }
+    bracket_tap_state.state = TD_NONE;
+}
+
+void angle_finished(tap_dance_state_t *state, void *user_data) {
+    angle_tap_state.state = cur_dance(state);
+    switch (angle_tap_state.state) {
+        case TD_SINGLE_TAP: register_code16(KC_LT); break;
+        case TD_SINGLE_HOLD: register_code16(KC_GT); break;
+        case TD_DOUBLE_TAP: tap_code16(KC_LT); tap_code16(KC_GT); register_code(KC_LEFT); break;
+        default: break;
+    }
+}
+
+void angle_reset(tap_dance_state_t *state, void *user_data) {
+    switch (angle_tap_state.state) {
+        case TD_SINGLE_TAP: unregister_code16(KC_LT); break;
+        case TD_SINGLE_HOLD: unregister_code16(KC_GT); break;
+        case TD_DOUBLE_TAP: unregister_code(KC_LEFT); break;
+        case TD_DOUBLE_HOLD: break;
+        case TD_DOUBLE_SINGLE_TAP: break;
+        default: break;
+    }
+    angle_tap_state.state = TD_NONE;
+}
+
 tap_dance_action_t tap_dance_actions[] = {
-  [TD_AA] = ACTION_TAP_DANCE_TAP_HOLD(SE_AA, S(SE_AA)),
-  [TD_AE] = ACTION_TAP_DANCE_TAP_HOLD(SE_AE, S(SE_AE)),
-  [TD_OE] = ACTION_TAP_DANCE_TAP_HOLD(SE_OE, S(SE_OE))
+  [TD_E_AE] = ACTION_TAP_DANCE_TAP_HOLD(KC_E, RALT(KC_A)),
+  [TD_W_AA] = ACTION_TAP_DANCE_TAP_HOLD(KC_W, RALT(KC_W)),
+  [TD_O_OE] = ACTION_TAP_DANCE_TAP_HOLD(KC_O, RALT(KC_O)),
+  [TD_ENTER] = ACTION_TAP_DANCE_TAP_HOLD(KC_ENT, LCTL(KC_ENT)),
+  [TD_PAREN] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, paren_finished, paren_reset),
+  [TD_CURLY] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, curly_finished, curly_reset),
+  [TD_BRACKET] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, bracket_finished, bracket_reset),
+  [TD_ANGLE] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, angle_finished, angle_reset),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     tap_dance_action_t *action;
 
     switch (keycode) {
-        case TD(TD_AA):
-        case TD(TD_AE):
-        case TD(TD_OE):
+        case TD(TD_E_AE):
+        case TD(TD_W_AA):
+        case TD(TD_O_OE):
+        case TD(TD_ENTER):
             action = &tap_dance_actions[QK_TAP_DANCE_GET_INDEX(keycode)];
             if (!record->event.pressed && action->state.count && !action->state.finished) {
                 tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)action->user_data;
