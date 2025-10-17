8a71ae1081025e76bd241d5a983951bf4054573a
Anton Österberg <anton.osterberg@protonmail.com>
2025-10-17 11:44:23 +0200

    back to home row modifiers

--- keyboards/splitkb/aurora/sweep/keymaps/qwerty_lhrm/keymap.c
+++ keyboards/splitkb/aurora/sweep/keymaps/qwerty_lhrm/keymap.c
@@ -86,15 +86,15 @@ enum {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_Q,    KC_W, KC_E, KC_R, KC_T,                                                KC_Y, KC_U, KC_I, KC_O, KC_P,
-        KC_A, KC_S, KC_D, KC_F, KC_G,                                                   KC_H, KC_J, KC_K, KC_L, KC_SCLN,
-        KC_Z, KC_X, KC_C, KC_V, KC_B,                                                   KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
+        KC_Q, TD(TD_W_AA), TD(TD_E_AE), KC_R, KC_T,                                     KC_Y, KC_U, KC_I, TD(TD_O_OE), KC_P,
+        LGUI_T(KC_A), LSFT_T(KC_S), LALT_T(KC_D), LCTL_T(KC_F), KC_G,      KC_H, RCTL_T(KC_J), LALT_T(KC_K), RSFT_T(KC_L), RGUI_T(KC_SCLN),
+        KC_Z, KC_X, KC_C, KC_V, KC_B,                                                   KC_N, KC_M, KC_COMM, KC_DOT, KC_TAB,
         LT(SYM, KC_ESC), LT(MOD, KC_SPC),                                              LT(MOD, KC_BSPC), LT(SYM, KC_ENT)
     ),
 
     [1] = LAYOUT(
            KC_1,     KC_2,       KC_3,       KC_4,    KC_5,                                      KC_6,       KC_7,       KC_8,       KC_9,       KC_0,
-        KC_LGUI,  KC_LSFT,    LALT_T(KC_LEFT), LCTL_T(KC_RGHT),  KC_DEL,                      KC_BSPC,    RCTL_T(KC_DOWN), LALT_T(KC_UP),   KC_RSFT,    KC_RGUI,
+        KC_LGUI,  KC_LSFT,    KC_LALT,    KC_LCTL,    XXXX,                                   KC_LEFT,    KC_DOWN,      KC_UP,   KC_RIGHT,       ____,
           KC_F1,    KC_F2,      KC_F3,      KC_F4,   KC_F5,                                     KC_F6,      KC_F7,      KC_F8,      KC_F9,     KC_F10,
                                                       ____,    QK_LAYER_LOCK,              QK_LAYER_LOCK,    ____
       ),
@@ -129,10 +129,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 // Combos
-const uint16_t PROGMEM switcher[] = {KC_F, KC_J, COMBO_END};
+const uint16_t PROGMEM switcher[] = {LCTL_T(KC_F), RCTL_T(KC_J), COMBO_END};
 const uint16_t PROGMEM launcher[] = {KC_G, KC_H, COMBO_END};
 //const uint16_t PROGMEM nav_lock[] = {LT(1, KC_SPC), LSFT_T(KC_BSPC), COMBO_END};
-const uint16_t PROGMEM caps_word[] = {KC_S, KC_L, COMBO_END};
+const uint16_t PROGMEM caps_word[] = {LSFT_T(KC_S), RSFT_T(KC_L), COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(switcher, LGUI(KC_H)),
