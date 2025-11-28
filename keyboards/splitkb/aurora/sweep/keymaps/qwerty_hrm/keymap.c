3f8ad5b3feae2edd0b21de51aff60277355540bc
Anton Österberg <anton.osterberg@protonmail.com>
2025-11-28 16:30:33 +0100

    Revert "no HRM"
    This reverts commit fe7bce4ad3b73222295f0d57742a3673be9582c3.

--- keyboards/splitkb/aurora/sweep/keymaps/qwerty_hrm/keymap.c
+++ keyboards/splitkb/aurora/sweep/keymaps/qwerty_hrm/keymap.c
@@ -80,22 +80,21 @@ enum {
     BASE = 0,
     NAV = 1,
     SYM = 2,
-    MOD = 3,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_Q, KC_W, KC_E, KC_R, KC_T,          KC_Y, KC_U, KC_I,    KC_O,   KC_P,
-        KC_A, KC_S, KC_D, KC_F, KC_G,          KC_H, KC_J, KC_K,    KC_L,   KC_ENT,
-        KC_Z, KC_X, KC_C, KC_V, KC_B,          KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,
-        LT(SYM, KC_ESC), LT(NAV, KC_SPC),      MO(MOD), LT(SYM, KC_TAB)
+        KC_Q, TD(TD_W_AA), TD(TD_E_AE), KC_R, KC_T,                                     KC_Y, KC_U, KC_I, TD(TD_O_OE), KC_P,
+        LGUI_T(KC_A), LSFT_T(KC_S), LALT_T(KC_D), LCTL_T(KC_F), KC_G,      KC_H, RCTL_T(KC_J), LALT_T(KC_K), RSFT_T(KC_L), RGUI_T(KC_ENT),
+        KC_Z, KC_X, KC_C, KC_V, KC_B,                                     KC_N, KC_M,  KC_COMM,  KC_DOT, KC_SLSH,
+                                    LT(NAV, KC_ESC), LT(SYM, KC_SPC),          LT(SYM, KC_BSPC), KC_TAB
     ),
 
     [1] = LAYOUT(
         KC_F1,    KC_F2,      KC_F3,      KC_F4,   KC_F5,                                     KC_F6,      KC_F7,      KC_F8,      KC_F9,     KC_F10,
         KC_LGUI,  KC_LSFT,    LALT_T(KC_LEFT),    LCTL_T(KC_RGHT),    KC_HOME,              KC_END,    RCTL_T(KC_DOWN),      LALT_T(KC_UP),   KC_RSFT,  KC_RGUI,
         KC_F11,   KC_F12,     XXXX,        XXXX,    XXXX,                                      XXXX,       XXXX,       XXXX,       XXXX,       XXXX,
-                                                                        ____,    ____,      KC_BSPC, KC_DEL
+                                                                        ____,    ____,              ____, ____
     ),
 
 
@@ -141,18 +140,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_1,          KC_2,          KC_3,          KC_4,          KC_5,                            KC_6,          KC_7,          KC_8,          KC_9,          KC_0,
         //        `              +              -              =              '                                "              :              _              ?              ;
                 KC_GRV,        KC_PLUS,       KC_MINS,       KC_EQL,     TD(TD_QUOT),                     TD(TD_DQUO),      KC_COLN,       KC_UNDS,       KC_QUES,       KC_SCLN,
-        //        \              ~              |             ( )            [ ]                              < >            { }             å              ä              ö
-                KC_BSLS,       KC_TILD,       KC_PIPE,    TD(TD_PAREN),  TD(TD_BRACKET),                  TD(TD_ANGLE),  TD(TD_CURLY), RALT(KC_W),    RALT(KC_A),    RALT(KC_O),
+        //        \              ~              |             ( )            [ ]                              < >            { }            ,                .             /
+                KC_BSLS,       KC_TILD,       KC_PIPE,    TD(TD_PAREN),  TD(TD_BRACKET),                  TD(TD_ANGLE),  TD(TD_CURLY),     ____,           ____,         ____,
         //
                                                                     ____,    ____,                ____,       ____
     ),
 
-   [3] = LAYOUT(
-       ____,  ____,  ____,  ____,  ____,                               ____,  ____,  ____,  ____,  ____,
-       OSM(MOD_LGUI),  OSM(MOD_LSFT),  OSM(MOD_LALT),  OSM(MOD_LCTL),  ____,                  ____,  OSM(MOD_RCTL), OSM(MOD_LALT), OSM(MOD_RSFT), OSM(MOD_RGUI),
-       ____,  ____,  ____,  ____,  ____,                               ____,  ____,  ____,  ____,  ____,
-                            ____,  ____,                               ____,  ____
-   ),
+
+   // [4] = LAYOUT(
+   //     ____,  ____,  KC_DEL,  ____,  ____,                               ____,  ____,  TG(NAV),  ____,  ____,
+   //     ____,  ____,  KC_LEFT,  KC_RGHT,  ____,                      KC_BSPC,  KC_DOWN,  KC_UP,  ____,  ____,
+   //     ____,  ____,  ____,  ____,  ____,                               ____,  ____,  ____,  ____,  ____,
+   //                          ____,  ____,                               ____,  ____
+   // ),
 // Layer template, copy paste as needed
 //    [4] = LAYOUT(
 //        XXXX,  XXXX,  XXXX,  XXXX,  XXXX,                               XXXX,  XXXX,  XXXX,  XXXX,  XXXX,
@@ -164,10 +164,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 // Combos
-const uint16_t PROGMEM switcher[] = {KC_F, KC_J, COMBO_END};
+const uint16_t PROGMEM switcher[] = {LCTL_T(KC_F), RCTL_T(KC_J), COMBO_END};
 const uint16_t PROGMEM launcher[] = {KC_G, KC_H, COMBO_END};
 //const uint16_t PROGMEM nav_lock[] = {LT(1, KC_SPC), LSFT_T(KC_BSPC), COMBO_END};
-const uint16_t PROGMEM caps_word[] = {KC_S, KC_L, COMBO_END};
+const uint16_t PROGMEM caps_word[] = {LSFT_T(KC_S), RSFT_T(KC_L), COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(switcher, LGUI(KC_H)),
