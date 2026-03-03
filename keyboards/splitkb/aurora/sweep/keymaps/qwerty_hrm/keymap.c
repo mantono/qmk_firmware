901bbdc0399c173e07de496e8d0240a4da5a159c
Anton Österberg <anton.osterberg@protonmail.com>
2026-03-03 23:38:46 +0100

    move num to nav layer

--- keyboards/splitkb/aurora/sweep/keymaps/qwerty_hrm/keymap.c
+++ keyboards/splitkb/aurora/sweep/keymaps/qwerty_hrm/keymap.c
@@ -112,7 +112,7 @@ enum {
     BASE = 0,
     NAV = 1,
     SYM = 2,
-    NUM = 3,
+    FUNC = 3,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -120,13 +120,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q, TD(TD_W_AA), TD(TD_E_AE), KC_R, KC_T,                                     KC_Y, KC_U, KC_I, TD(TD_O_OE), KC_P,
         LGUI_T(KC_A), LSFT_T(KC_S), LALT_T(KC_D), LCTL_T(KC_F), KC_G,      KC_H, RCTL_T(KC_J), LALT_T(KC_K), RSFT_T(KC_L), TD(TD_ENTER),
         KC_Z, KC_X, KC_C, KC_V, KC_B,                                     KC_N, KC_M,  KC_COMM,  KC_DOT, KC_SLSH,
-                                         KC_ESC, LT(NAV, KC_SPC),          LT(SYM, KC_TAB), MO(NUM)
+                                         KC_ESC, LT(NAV, KC_SPC),          LT(SYM, KC_TAB), LT(FUNC, QK_CAPS_WORD_TOGGLE)
     ),
 
     [1] = LAYOUT(
-          KC_F1,    KC_F2,      KC_F3,      KC_F4,   KC_F5,                                     KC_F6,      KC_F7,      KC_F8,      KC_F9,     KC_F10,
+            KC_1,  KC_2,  KC_3,  KC_4,  KC_5,                                                  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,
           KC_LGUI,  KC_LSFT,  LALT_T(KC_LEFT),  LCTL_T(KC_RGHT),  KC_DEL,                      KC_BSPC,  RCTL_T(KC_DOWN),  LALT_T(KC_UP), KC_RSFT, ____,
-          KC_F11,  SELLINE,  KC_HOME,   KC_END,    XXXX,                      XXXX,   KC_PGDN,   KC_PGUP,       XXXX,       KC_F12,
+          XXXX,  SELLINE,  KC_HOME,   KC_END,    XXXX,                                        XXXX,   KC_PGDN,   KC_PGUP,       XXXX,       XXXX,
                               ____,  ____,                               ____,  ____
     ),
 
@@ -178,8 +178,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [3] = LAYOUT(
          ____,  ____,  ____,  ____,  ____,         ____,  ____,  ____,  ____,  ____,
-         KC_1,  KC_2,  KC_3,  KC_4,  KC_5,         KC_6,  KC_7,  KC_8,  KC_9,  KC_0,
-         ____,  ____,  ____,  ____,  ____,         ____,  ____,  ____,  ____,  ____,
+        LGUI_T(KC_F1),  LSFT_T(KC_F2),  LALT_T(KC_F3),  LCTL_T(KC_F4),  KC_F5,         KC_F6,  RCTL_T(KC_F7),  LALT_T(KC_F8), RSFT_T(KC_F9),  RGUI_T(KC_F10),
+         KC_F11,  KC_F12,  ____,  ____,  ____,         ____,  ____,  ____,  ____,  ____,
                               ____,  ____,         ____,   ____
     ),
 
