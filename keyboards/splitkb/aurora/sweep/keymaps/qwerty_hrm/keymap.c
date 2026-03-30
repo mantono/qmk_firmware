21d86000ed78077419c3d5ba227ea785bdc3d436
Anton Österberg <anton.osterberg@protonmail.com>
2026-03-30 10:42:31 +0200

    move tab / back slash / shift tab / back space / enter

--- keyboards/splitkb/aurora/sweep/keymaps/qwerty_hrm/keymap.c
+++ keyboards/splitkb/aurora/sweep/keymaps/qwerty_hrm/keymap.c
@@ -117,13 +117,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q, TD(TD_W_AA), TD(TD_E_AE), KC_R, KC_T,                                KC_Y, KC_U, KC_I, TD(TD_O_OE), KC_P,
         LT(SYM, KC_A), LGUI_T(KC_S), LALT_T(KC_D), LCTL_T(KC_F), KC_G,                           KC_H, RCTL_T(KC_J), LALT_T(KC_K), RGUI_T(KC_L), LT(SYM, KC_SCLN),
         KC_Z, KC_X, KC_C, KC_V, KC_B,                                           KC_N, KC_M,  KC_COMM,  KC_DOT, TD(TD_ENTER),
-                            KC_ESC, LT(NAV, KC_SPC),          LSFT_T(KC_TAB), LSFT(KC_TAB)
+                            KC_ESC, LT(NAV, KC_SPC),          LSFT_T(KC_BSPC), TD(TD_ENTER)
     ),
 
     [1] = LAYOUT(
             KC_1,  KC_2,  KC_3,  KC_4,  KC_5,                                                  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,
-          XXXX,  KC_LGUI,  LALT_T(KC_LEFT),  LCTL_T(KC_RGHT),  KC_DEL,                      KC_BSPC,  RCTL_T(KC_DOWN),  LALT_T(KC_UP), KC_RGUI, XXXX,
-          XXXX,  SELLINE,  KC_HOME,   KC_END,    XXXX,                                        XXXX,   KC_PGDN,   KC_PGUP,       XXXX,       ____,
+          XXXX,  KC_LGUI,  LALT_T(KC_LEFT),  LCTL_T(KC_RGHT),  KC_TAB,                      LSFT(KC_TAB),  RCTL_T(KC_DOWN),  LALT_T(KC_UP), KC_RGUI, XXXX,
+          XXXX,  SELLINE,  KC_HOME,   KC_END,    XXXX,                                        XXXX,   KC_PGDN,   KC_PGUP,       XXXX,       KC_SLSH,
                               ____,  ____,                               ____,  ____
     ),
 
